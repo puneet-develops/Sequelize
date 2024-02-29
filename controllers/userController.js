910e782f3337c0a29e72090e0fae4c49403fbe0d
puneet-develops <puneet.k@antino.com>
2024-02-29 14:38:52 +0530

    crud hardcoded done

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,27 +1,114 @@
-var db=require('../models')
-var User =db.user;
-var Contact=db.contact;
+var db = require("../models");
+var User = db.user;
+var Contact = db.contact;
 
-var adduser=async(req,res)=>{
-//const jane = User.build({ firstName: "Jane" });
-const jane = await User.create({ firstName: "antino" ,lastName: "labs"});
-console.log(jane instanceof User); // true
-console.log(jane.firstName); // "Jane"
-//await jane.save();
-console.log('antino  was saved to the database!');
-res.status(200).json(jane.toJSON());
-}
-var addcontact=async(req,res)=>{
-//const antino = User.build({ firstName: "Jane" });
-const antino = await Contact.create({ firstName: "antino" ,lastName: "labs",phone:"1787872"});
-console.log(antino instanceof Contact); // true
-console.log(antino.phone); // "antno"
-//await antino.save();
-console.log('antino  was saved to the database!');
-res.status(200).json(antino.toJSON());
-}
+var adduser = async (req, res) => {
+  // //const jane = User.build({ firstName: "Jane" });
+  // const jane = await User.create({ firstName: "antino" ,lastName: "labs"});
+  // console.log(jane instanceof User); // true
+  // console.log(jane.firstName); // "Jane"
+  // //await jane.save();
+  // console.log('antino  was saved to the database!');
+  // res.status(200).json(jane.toJSON());
+  var postdata = req.body;
+  if(postdata.length>1){
+    var data=await User.bulkCreate(postdata);
+  }else{
+    var data = await User.create(postdata); // this is first step now get the values from request
+  }
+  res.status(200).json({ data: data });
+};
+var addcontact = async (req, res) => {
+//   //const antino = User.build({ firstName: "Jane" });
+//   const antino = await Contact.create({
+//     firstName: "antino",
+//     lastName: "labs",
+//     phone: "1787872",
+//   });
+//   console.log(antino instanceof Contact); // true
+//   console.log(antino.phone); // "antno"
+//   //await antino.save();
+//   console.log("antino  was saved to the database!");
+//   res.status(200).json(antino.toJSON());
+var postdata = req.body;
+if(postdata.length>1){
+    var data=await Contact.bulkCreate(postdata);
+  }else{
+    var data = await Contact.create(postdata); // this is first step now get the values from request
+  } // this is first step now get the values from request
+  res.status(200).json({ data: data });
+};
 
-module.exports={
-    adduser,
-    addcontact
-}
+const getcontacts = async (req, res) => {
+  const find = await Contact.findAll({});
+  res.status(200).json({ data: find });
+};
+const getcontact = async (req, res) => {
+  const find = await Contact.findOne({
+    where: {
+      id: req.params.id,
+    },
+  });
+  res.status(200).json({ data: find });
+};
+const getusers = async (req, res) => {
+  const find = await User.findAll({});
+  res.status(200).json({ data: find });
+};
+const getuser = async (req, res) => {
+  const find = await User.findOne({
+    where: {
+      id: req.params.id,
+    },
+  });
+  res.status(200).json({ data: find });
+};
+const deletecontact = async (req, res) => {
+    const find = await Contact.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json({ data: find });
+  };
+  const deleteuser = async (req, res) => {
+    const find = await User.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json({ data: find });
+  };
+  const patchuser = async (req, res) => {
+    var updatedbody=req.body;
+    const find = await User.update(updatedbody,{
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json({ data: find });
+  };
+  const patchcontact = async (req, res) => {
+    var updatedbody=req.body;
+    const find = await Contact.update(updatedbody,{
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json({ data: find });
+  };
+
+
+module.exports = {
+  adduser,
+  addcontact,
+  getusers,
+  getuser,
+  getcontacts,
+  getcontact,
+  deletecontact,
+  deleteuser,
+  patchuser,
+  patchcontact
+ 
+};
--- index.js
+++ index.js
@@ -8,8 +8,19 @@ app.use(bodyParser.json())
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
-app.get('/add',userctrl.adduser)
-app.get('/contact',userctrl.addcontact)
+app.post('/adduser',userctrl.adduser);
+app.post('/addcontact',userctrl.addcontact);
+app.get('/users',userctrl.getusers);
+app.get ('/users/:id',userctrl.getuser);
+app.get('/contacts',userctrl.getcontacts);
+app.get ('/contacts/:id',userctrl.getcontact);
+app.delete ('/users/:id',userctrl.deleteuser);
+app.delete ('/contacts/:id',userctrl.deletecontact);
+app.patch ('/users/:id',userctrl.patchuser);
+app.patch ('/contacts/:id',userctrl.patchcontact);
+
+
+
 
 app.listen(3000,()=>{
     console.log("app will run on 3000");
